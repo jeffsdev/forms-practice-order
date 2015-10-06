51bc7ec842a14dc5ab9c2fd871f7f0d45c75eb10
jeff seymour & quinn oaks <student@epicodus.com>
2015-10-05 21:00:52 -0700

    add jquery scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,17 @@
 $(document).ready(function() {
 
+  $(".btn").click(function(event) {
+    var nameInput = $("#name").val();
+    var addressInput = $("#address").val();
 
+    $(".name").text(nameInput);
+    $(".address").text(addressInput);
+
+
+    event.preventDefault();
+
+    $("#receipt").show();
+
+  });
 
 });
